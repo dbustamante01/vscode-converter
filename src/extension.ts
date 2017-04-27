bddd01050d2cc6438bc9b5606566527a866b814a
Cahit Beyaz <CahitB1@verifone.com>
2017-04-27 11:18:29 +0300

    add hextoascii and asciitohex functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,24 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+//http://stackoverflow.com/questions/3745666/how-to-convert-from-hex-to-ascii-in-javascript
+function a2hex(str) {
+  var arr = [];
+  for (var i = 0, l = str.length; i < l; i ++) {
+    var hex = Number(str.charCodeAt(i)).toString(16);
+    arr.push(hex);
+  }
+  return arr.join('');
+}
+
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -26,7 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
         var text = editor.document.getText(selection);
 
         editor.edit(function (builder) {
-            builder.replace(selection, "test");
+            builder.replace(selection, hex2a(text));
         })
 
     });
